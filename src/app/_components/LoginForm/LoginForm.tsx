0651c3fb9ef4e6e2362e0588e60fbe9c0bb48894
OJ Abba <phavorabba@gmail.com>
2024-10-23 23:50:51 +0100

    Create LoginForm component

---
+++ src/app/_components/LoginForm/LoginForm.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { handleLoginForm } from './actions'
+import { useActionState } from 'react'
+import { useFormStatus } from 'react-dom'
+import authStyles from '@/styles/authStyles.module.scss'
+import Button from '@/components/Button'
+import Link from 'next/link'
+import PasswordInput from '@/components/PasswordInput'
+
+export default function LoginForm() {
+  const initialState: ActionResponse = {
+    success: false,
+    message: '',
+    isIdle: true,
+  }
+  const [formState, formAction] = useActionState(handleLoginForm, initialState)
+
+  return (
+    <form action={formAction} className={authStyles.authFormBody}>
+      {!formState.isIdle && (
+        <div className={authStyles.authFormBodyFeedback}>
+          {formState.message}
+        </div>
+      )}
+      <input
+        className={authStyles.authFormBodyInput}
+        type="email"
+        name="email"
+        placeholder="Email"
+      />
+      <PasswordInput
+        className={authStyles.authFormBodyInput}
+        name="password"
+        placeholder="Password"
+      />
+
+      <Link href={'#'} className={authStyles.authFormBodyLink}>
+        FORGOT PASSWORD?
+      </Link>
+
+      <SubmitButton />
+    </form>
+  )
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus()
+  return (
+    <Button type="submit" disabled={pending} isBold isWide>
+      LOG IN
+    </Button>
+  )
+}
